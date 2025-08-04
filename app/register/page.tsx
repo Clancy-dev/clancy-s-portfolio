369d1899c771102739034dd5f689872fbb728418
Clancy-dev <clancyro1789@gmail.com>
2025-08-04 10:46:46 +0300

    the

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,6 +1,4 @@
-
-
-import { RegisterPage } from "@/components/register-page";
+import { RegisterPage } from "@/components/register-page"
 
 export default function Register() {
   return <RegisterPage />
