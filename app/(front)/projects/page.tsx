7d009227a368379ab3ab66b7af396f7c0de5a181
clancyssekisambu-afk <clancyssekisambu@gmail.com>
2026-05-25 09:58:45 +0300

    the

--- app/(front)/projects/page.tsx
+++ app/(front)/projects/page.tsx
@@ -302,6 +302,7 @@ export default function ProjectsPage() {
                 >
                   {getCategoryLabel(cat)}
                 </button>
+                // sample
               ))}
             </div>
           </div>
